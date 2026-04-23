d438bcf54e9d96ae9833e0732e0ed46c551ed37b
kirill <kirill56gaponenko@gmail.com>
2026-04-24 01:36:59 +0300

    Replace student management with post management module, including controllers, routes, and MongoDB configuration.

--- src/server.js
+++ src/server.js
@@ -7,8 +7,16 @@ import errorHandler from "./middlewares/error.middleware.js";
 import authentication from "./middlewares/authentication.middleware.js";
 import {createAdmin} from "./configuration/initAdmin.js";
 import authorizationRouter  from "./routes/authorization.routes.js";
+import cors  from "cors";
 
 const app = express();
+
+const corsOptions = {
+    origin: ['http://localhost:8080'],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(authentication)
